1fbd268ca2534ae6cb9f8a439e359e7a6bd0f670
lanninisgarden <kevinjohn.mcclung@gmail.com>
2021-06-26 18:37:02 -0400

    Parse strings.xml and add EditText to top of UI.

--- app/src/main/java/com/sonnysappbakery/studychinese/MainActivity.java
+++ app/src/main/java/com/sonnysappbakery/studychinese/MainActivity.java
@@ -3,14 +3,38 @@ package com.sonnysappbakery.studychinese;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] sentenceArray;
+    List<String> sentenceList;
+    List<List<String>> lessonList;
+    int numberOfSentences;
+    int numberOfLessons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sentenceArray = getResources().getStringArray(R.array.course);
+        sentenceList = new ArrayList<>(Arrays.asList(sentenceArray));
+        lessonList = new ArrayList<>();
+        numberOfSentences = sentenceList.size();
+        numberOfLessons = numberOfSentences / 10;
+
+        int index = 0;
+        for(int i=0; i<numberOfLessons; i++) {
+            List<String> lesson = new ArrayList<>();
+            for(int j=0; j<10; j++) {
+                lesson.add(sentenceList.get(index));
+                index++;
+            }
+            lessonList.add(lesson);
+        }
     }
 }
