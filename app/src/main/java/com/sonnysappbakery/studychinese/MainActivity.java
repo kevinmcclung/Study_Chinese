8f065f824d61ba9ea9c6de3398c3f6b1ee253c40
lanninisgarden <kevinjohn.mcclung@gmail.com>
2021-06-29 21:37:27 -0400

    Add level number to each lesson title

--- app/src/main/java/com/sonnysappbakery/studychinese/MainActivity.java
+++ app/src/main/java/com/sonnysappbakery/studychinese/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
     TextView textView8;
     TextView textView9;
     TextView textView10;
+    TextView textView11;
     ScrollView scrollView;
 
     @Override
@@ -55,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
         textView8 = findViewById(R.id.textView8);
         textView9 = findViewById(R.id.textView9);
         textView10 = findViewById(R.id.textView10);
+        textView11 = findViewById(R.id.textView11);
         scrollView = findViewById(R.id.scroll);
 
         sentenceArray = getResources().getStringArray(R.array.course);
@@ -96,18 +98,28 @@ public class MainActivity extends AppCompatActivity {
         String text = getString(R.string.lesson_number) + lessonNumber + " " + getString(R.string.of) + " " + numberOfLessons;
         textView.setText(text);
 
-        textView1.setText(sentenceList.get((lessonNumber - 1) * 10));
-        textView2.setText(sentenceList.get(1 + (lessonNumber - 1) * 10));
-        textView3.setText(sentenceList.get(2 + (lessonNumber - 1) * 10));
-        textView4.setText(sentenceList.get(3 + (lessonNumber - 1) * 10));
-        textView5.setText(sentenceList.get(4 + (lessonNumber - 1) * 10));
-        textView6.setText(sentenceList.get(5 + (lessonNumber - 1) * 10));
-        textView7.setText(sentenceList.get(6 + (lessonNumber - 1) * 10));
-        textView8.setText(sentenceList.get(7 + (lessonNumber - 1) * 10));
-        textView9.setText(sentenceList.get(8 + (lessonNumber - 1) * 10));
-        textView10.setText(sentenceList.get(9 + (lessonNumber - 1) * 10));
+        text = getString(R.string.lesson_title) + " " + lessonNumber + ", Level ";
+        if(lessonNumber <= 15) text = text + "1";
+        else if(lessonNumber <= 30) text = text + "2";
+        else if(lessonNumber <= 60) text = text + "3";
+        else if(lessonNumber <= 120) text = text + "4";
+        else if(lessonNumber <= 250) text = text + "5";
+        else if (lessonNumber <= 500) text = text + "6";
+        else text = text + "7";
+
+        textView1.setText(text);
+        textView2.setText(sentenceList.get((lessonNumber - 1) * 10));
+        textView3.setText(sentenceList.get(1 + (lessonNumber - 1) * 10));
+        textView4.setText(sentenceList.get(2 + (lessonNumber - 1) * 10));
+        textView5.setText(sentenceList.get(3 + (lessonNumber - 1) * 10));
+        textView6.setText(sentenceList.get(4 + (lessonNumber - 1) * 10));
+        textView7.setText(sentenceList.get(5 + (lessonNumber - 1) * 10));
+        textView8.setText(sentenceList.get(6 + (lessonNumber - 1) * 10));
+        textView9.setText(sentenceList.get(7 + (lessonNumber - 1) * 10));
+        textView10.setText(sentenceList.get(8 + (lessonNumber - 1) * 10));
+        textView11.setText(sentenceList.get(9 + (lessonNumber - 1) * 10));
 
         scrollView.fullScroll(View.FOCUS_UP); //scroll back up to top
-        scrollView.fullScroll(View.FOCUS_UP); //scroll back up to top
+        scrollView.fullScroll(View.FOCUS_UP); //second call needed to prevent incomplete scroll
     }
 }
